7503aef736a08fd55d0f0e4a2299f3a5c3ee7f54
angod <manofsimplepleasures@protonmail.com>
2020-03-25 22:21:26 +0200

    impelent full snake moving

--- snake.js
+++ snake.js
@@ -29,11 +29,12 @@ const
  * 4: BOTTOM
  */
 let
-  LEFT   = 1,
-  RIGHT  = 2,
-  TOP    = 3,
-  BOTTOM = 4,
-  DIRECTION  = LEFT;
+  PAUSE  = 0;
+  RIGHT  = 1,
+  BOTTOM = 2,
+  LEFT   = 3,
+  TOP    = 4,
+  DIRECTION  = RIGHT;
 
 // grid size __35x25__
 let drawGrid = () => {
@@ -77,30 +78,70 @@ const moveSnake = () => {
   prevHead = snake[0];
   // console.log("pHead: ", prevHead);
 
-  // if snake head === GRID_RB && direction === RIGHT
-  if (prevHead.end.x === GRID_RIGHT_BORDER && DIRECTION === LEFT) {
-    console.log("snake head === GRID_RB");
-    nextHead = {
-      start: {
-        x: CANVAS_HORIZPAD,
-        y: prevHead.start.y
-      },
-      end: {
-        x: CANVAS_HORIZPAD + CELL,
-        y: prevHead.end.y
+  switch (DIRECTION) {
+    // left
+    case LEFT:
+      if (prevHead.start.x !== GRID_LEFT_BORDER) {
+        nextHead = {
+          start: { x: prevHead.start.x - CELL, y: prevHead.start.y },
+          end:   { x: prevHead.end.x - CELL,   y: prevHead.end.y   }
+        };
+      } else {
+        console.log("snake head === GRID_LB");
+        nextHead = {
+          start: { x: cvsWidth - CANVAS_HORIZPAD - CELL - 1, y: prevHead.start.y },
+          end:   { x: cvsWidth - CANVAS_HORIZPAD- 1,         y: prevHead.end.y   }
+        };
       }
-    }
-  } else {
-    nextHead = {
-      start: {
-        x: prevHead.start.x + CELL,
-        y: prevHead.start.y
-      },
-      end: {
-        x: prevHead.end.x + CELL,
-        y: prevHead.end.y
+      break;
+
+    // right
+    case RIGHT:
+      if (prevHead.end.x !== GRID_RIGHT_BORDER) {
+        nextHead = {
+          start: { x: prevHead.start.x + CELL, y: prevHead.start.y },
+          end:   { x: prevHead.end.x + CELL,   y: prevHead.end.y   }
+        };
+      } else {
+        console.log("snake head === GRID_RB");
+        nextHead = {
+          start: { x: CANVAS_HORIZPAD,        y: prevHead.start.y },
+          end:   { x: CANVAS_HORIZPAD + CELL, y: prevHead.end.y   }
+        };
       }
-    };
+      break;
+
+    // top
+    case TOP:
+      if (prevHead.start.y !== GRID_TOP_BORDER) {
+        nextHead = {
+          start: { x: prevHead.start.x, y: prevHead.start.y - CELL },
+          end:   { x: prevHead.end.x,   y: prevHead.end.y - CELL   }
+        };
+      } else {
+        console.log("snake head === GRID_TB");
+        nextHead = {
+          start: { x: prevHead.start.x, y: cvsHeight - CANVAS_VERTPAD - CELL - 1 },
+          end:   { x: prevHead.end.x,   y: cvsHeight - CANVAS_VERTPAD - 1        }
+        };
+      }
+      break;
+
+    // bottom
+    case BOTTOM:
+      if (prevHead.end.y !== GRID_BOTTOM_BORDER) {
+        nextHead = {
+          start: { x: prevHead.start.x, y: prevHead.start.y + CELL },
+          end:   { x: prevHead.end.x,   y: prevHead.end.y + CELL   }
+        };
+      } else {
+        console.log("snake head === GRID_BB");
+        nextHead = {
+          start: { x: prevHead.start.x, y: CANVAS_VERTPAD        },
+          end:   { x: prevHead.end.x,   y: CANVAS_VERTPAD + CELL }
+        };
+      }
+      break;
   }
 
   // console.log("nHead: ", nextHead);
@@ -110,8 +151,31 @@ const moveSnake = () => {
   sceneCtx.clearRect(0, 0, gridCvs.width, gridCvs.height);
 };
 
-// rewrite for compact view
-let printSnake = (snake) => {
+const changeDirection = (e) => {
+  // console.log(e.code);
+  switch (e.code) {
+    case "ArrowLeft":
+      if (DIRECTION !== LEFT && DIRECTION !== RIGHT) DIRECTION = LEFT;
+      break;
+
+    case "ArrowRight":
+      if (DIRECTION !== RIGHT && DIRECTION !== LEFT) DIRECTION = RIGHT;
+      break;
+
+    case "ArrowUp":
+      if (DIRECTION !== TOP && DIRECTION !== BOTTOM) DIRECTION = TOP;
+      break;
+
+    case "ArrowDown":
+      if (DIRECTION !== BOTTOM && DIRECTION !== TOP) DIRECTION = BOTTOM;
+      break;
+  }
+
+  console.log(`DIRECTION: ${DIRECTION}`);
+};
+
+// rewrite for compact output
+const printSnake = (snake) => {
   console.log("##############################################");
   for (const { start: { x: startX, y: startY}, end: { x: endX, y: endY } } of snake) {
     console.table([["start", startX, startY], ["end", endX, endY]]);
@@ -126,7 +190,7 @@ const delay = (n) => {
       done();
     }, n);
   });
-}
+};
 
 let counter = 0;
 
@@ -134,13 +198,15 @@ const gameloop = async () => {
   drawSnake();
 
   // if (++counter <= 35) {
-    await delay(400);
+    await delay(250);
     moveSnake();
     // console.log("gameloop tick: ", counter);
   // }
 
   requestAnimationFrame(gameloop);
 };
+
+document.addEventListener("keydown", changeDirection);
 
 drawGrid();
 gameloop();
